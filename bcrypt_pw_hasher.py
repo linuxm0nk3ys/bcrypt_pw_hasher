2a96b7dcf17f1024c39eb6245aaa40ef1722e86c
Łukasz Kostka <ukasz83@gmail.com>
2014-02-24 20:48:26 +0100

    changed to propmt for password writing in cli will store password in plain text in shell history file

--- bcrypt_pw_hasher.py
+++ bcrypt_pw_hasher.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import bcrypt
+import getpass
 from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-u", "--user", dest="username", help="Username")
-parser.add_option("-p", "--password", dest="password", help="Password which will be hashed using bcrypt")
 parser.add_option("-w", "--workfactor", dest="workfactor", default="7", help="Workfactor of bcrypt, the higher the more secure [Default is %default]")
 
+password = getpass.getpass()
+
 (options, args) = parser.parse_args()
-if options.username == None or options.password == None:
+if options.username == None or password == None:
     parser.print_help()
 else:
-    hashed = bcrypt.hashpw(options.password,bcrypt.gensalt(int(options.workfactor)))
+    hashed = bcrypt.hashpw(password,bcrypt.gensalt(int(options.workfactor)))
     print "%s:%s" % (options.username,hashed)

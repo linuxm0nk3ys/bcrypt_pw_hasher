ff79679a96ce2eea86640b5e2a479e7f55043226
funkym0nk3y <funkym0nk3y@linuxm0nk3ys.vpn>
2014-02-25 13:42:32 +0100

    added verification of password input (2nd input of password).

--- bcrypt_pw_hasher.py
+++ bcrypt_pw_hasher.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 import bcrypt
-import getpass
+import getpass as gp
 from optparse import OptionParser
+
 
 parser = OptionParser()
 parser.add_option("-u", "--user", dest="username", help="Username")
 parser.add_option("-w", "--workfactor", dest="workfactor", default="7", help="Workfactor of bcrypt, the higher the more secure [Default is %default]")
 
-password = getpass.getpass()
-
 (options, args) = parser.parse_args()
-if options.username == None or password == None:
+if (options.username == None):
     parser.print_help()
 else:
-    hashed = bcrypt.hashpw(password,bcrypt.gensalt(int(options.workfactor)))
-    print "%s:%s" % (options.username,hashed)
+    password = gp.getpass()
+    verify = gp.getpass("Verify Password: ")
+    if (password == verify):
+        hashed = bcrypt.hashpw(password,bcrypt.gensalt(int(options.workfactor)))
+        print "%s:%s" % (options.username,hashed)
+    else:
+        print "Password doesn't match!"
